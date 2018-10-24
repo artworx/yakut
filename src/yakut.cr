54fc9e11df08a8d53515e94725a7e8c20baef057
Alexandru Keszeg <akeszeg@pitechnologies.ro>
2018-10-24 15:57:23 +0300

    override empty nil serialization from "none" to ""

--- src/yakut.cr
+++ src/yakut.cr
@@ -1,6 +1,12 @@
 require "crinja"
 require "./yakut/*"
 
+struct Crinja::Finalizer
+  protected def stringify(raw : Nil)
+    @io << ""
+  end
+end
+
 # TODO: Write documentation for `Yakut`
 module Yakut
   # TODO: Put your code here
